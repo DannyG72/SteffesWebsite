d3fbd50cadd146e3a3e1ea93c340dfc39cf1cbcd
SteffesLaptop <35386904+DannyG72@users.noreply.github.com>
2019-05-16 16:18:12 -0500

    Removed unneccessary processing.

--- SteffesUtilities.js
+++ SteffesUtilities.js
@@ -54,50 +54,6 @@
     adminButton.addEventListener("click", function() {openInNewTab('https://steffesgroup.com/Admin/Auctions');});
     document.body.appendChild(adminButton);
 
-    //Takes the lotIDS variable from the SteffesGroup website and splits it into a array to be used for the options in the selection list
-    var lotIDS = lotids.split('-');
-    lotIDS.pop();
-    var arrayOpts = lotIDS;
-
-    //Creates the selectList object for the auction lots.
-    var selectList = document.createElement("SELECT");
-    selectList.style = "top:12px;Right:388px;position:fixed;z-index: 9999";
-    selectList.id = "mySelect";
-    if (arrayOpts.length > 0) document.body.appendChild(selectList);
-
-    //Create and append the options to the select list.
-    for (var i = 0; i < arrayOpts.length; i++) {
-        var option = document.createElement("option");
-        var lotIDHTML = document.getElementById('lot-container-'+arrayOpts[i]);
-        var lotNumber = lotIDHTML.querySelector(`[id*='Lot-']`).outerHTML;
-        var tmpStr = lotNumber;
-        var tmpListCurrentLot = tmpStr.split('<span id="');
-        var tmpStrCurrentLot = tmpListCurrentLot.slice(1);
-        var currentLotlist = tmpStrCurrentLot.pop(0);
-        var tmpListCurrentLot2 = currentLotlist.split('" class="lot-number-heading');
-        var currentLot = tmpListCurrentLot2[0];
-
-
-        option.value = currentLot;
-        option.text = currentLot;
-        if (arrayOpts.length > 0) selectList.appendChild(option);}
-
-    //Create and append the Go button for the lot listing
-    var lotBackendGo = document.createElement("Button");
-    lotBackendGo.innerHTML = "Go";
-    lotBackendGo.id="SearhBidderButton";
-    lotBackendGo.style = "top:11px;right:349px;position:fixed;z-index: 9999";
-    lotBackendGo.style.background = "linear-gradient(to bottom, #fefdfd 0%,#b2b2b2 100%)";
-    lotBackendGo.style.border = "2px solid #a1a0a0";
-    lotBackendGo.addEventListener("click", function() {
-        var selected = selectList.selectedOptions[0].value;
-        var html = content.outerHTML;
-        var splitHTML = (html.split(selected))[0];
-        var splitLots = splitHTML.split('datatablestart-lot-');
-        var currentAuc = splitLots.pop();
-        var foundLotID = currentAuc.split('" style="')[0];
-        var backendLotLink = 'https://steffesgroup.com/Admin/LotDetails?lotId='+foundLotID;
-        openInNewTab(backendLotLink);});
 
     //Checks to see if the current page is an auction page, if so, append auction specific buttons.
     if (currentPageUrl.startsWith('https://steffesgroup.com/Auction/AuctionDetails?Name=')) {
@@ -105,6 +61,50 @@
         if (arrayOpts.length > 0) document.body.appendChild(lotBackendGo);
         $(".advancedSearchBtn").hide();}})();
 
+        //Takes the lotIDS variable from the SteffesGroup website and splits it into a array to be used for the options in the selection list
+        var lotIDS = lotids.split('-');
+        lotIDS.pop();
+        var arrayOpts = lotIDS;
+
+        //Creates the selectList object for the auction lots.
+        var selectList = document.createElement("SELECT");
+        selectList.style = "top:12px;Right:388px;position:fixed;z-index: 9999";
+        selectList.id = "mySelect";
+        if (arrayOpts.length > 0) document.body.appendChild(selectList);
+
+        //Create and append the options to the select list.
+        for (var i = 0; i < arrayOpts.length; i++) {
+          var option = document.createElement("option");
+          var lotIDHTML = document.getElementById('lot-container-'+arrayOpts[i]);
+          var lotNumber = lotIDHTML.querySelector(`[id*='Lot-']`).outerHTML;
+          var tmpStr = lotNumber;
+          var tmpListCurrentLot = tmpStr.split('<span id="');
+          var tmpStrCurrentLot = tmpListCurrentLot.slice(1);
+          var currentLotlist = tmpStrCurrentLot.pop(0);
+          var tmpListCurrentLot2 = currentLotlist.split('" class="lot-number-heading');
+          var currentLot = tmpListCurrentLot2[0];
+
+
+          option.value = currentLot;
+          option.text = currentLot;
+          if (arrayOpts.length > 0) selectList.appendChild(option);}
+
+          //Create and append the Go button for the lot listing
+          var lotBackendGo = document.createElement("Button");
+          lotBackendGo.innerHTML = "Go";
+          lotBackendGo.id="SearhBidderButton";
+          lotBackendGo.style = "top:11px;right:349px;position:fixed;z-index: 9999";
+          lotBackendGo.style.background = "linear-gradient(to bottom, #fefdfd 0%,#b2b2b2 100%)";
+          lotBackendGo.style.border = "2px solid #a1a0a0";
+          lotBackendGo.addEventListener("click", function() {
+            var selected = selectList.selectedOptions[0].value;
+            var html = content.outerHTML;
+            var splitHTML = (html.split(selected))[0];
+            var splitLots = splitHTML.split('datatablestart-lot-');
+            var currentAuc = splitLots.pop();
+            var foundLotID = currentAuc.split('" style="')[0];
+            var backendLotLink = 'https://steffesgroup.com/Admin/LotDetails?lotId='+foundLotID;
+            openInNewTab(backendLotLink);});
 //Get the value from the bidder Search Bar
 function getSearchBidder() {
     var bidderString = document.getElementById("bidderSearch").value;
