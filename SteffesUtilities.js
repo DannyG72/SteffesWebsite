ea46a795fbc588e18a442d1974347d970187dd46
SteffesLaptop <35386904+DannyG72@users.noreply.github.com>
2019-05-31 09:46:35 -0500

    cmt

--- SteffesUtilities.js
+++ SteffesUtilities.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steffesgroup Utilities
 // @namespace    https://github.com/DannyG72
-// @version      2.6
+// @version      2.7
 // @description  Adds a few quick-link buttons to the Steffes Group website for quick assistance and admin purposes. Quickly Search Bidders, Launch the Auction Backend, and edit an auction's lots.
 // @author       Daniel Glynn
 // @match        https://steffesgroup.com/*
